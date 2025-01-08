4ae9f51b7064fbf1cfd52971b85bb756fd5d71fa
Steven Manz <128404387+ManzCreations@users.noreply.github.com>
2025-01-07 20:55:12 -0500

    Update to real email.

--- app/js/config.js
+++ app/js/config.js
@@ -6,7 +6,7 @@ const config = {
     // EmailJS Configuration
     EMAIL: {
         PUBLIC_KEY: 'DD2qYtcM8ubvw2VLB',
-        SERVICE_ID: 'service_rojg3ef',
+        SERVICE_ID: 'service_cdzbqbe',
         TEMPLATES: {
             QUICK: 'template_562otjh',
             FULL: 'template_9wcbbkd'
